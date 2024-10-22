9547ae3c513ad3b3c29464ed94cbc1d41710e65c
Sebvanderhaar <seb@bigbeard.co.uk>
2024-10-22 12:52:00 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -12,16 +12,20 @@ def GetAxisOfGivenLength(array: np.ndarray, length: float) -> int:
         if array.shape[i] == length:
             return i
 
-def CoulombForceField(r: np.ndarray, chargeQ: float, chargePos: np.ndarray) -> np.ndarray:
+
+def CoulombForceField(r: np.ndarray, chargeQ: float,
+                      chargePos: np.ndarray) -> np.ndarray:
     axis: int = GetAxisOfGivenLength(r, 2)
     k: float = chargeQ / (4 * cnst.pi * cnst.epsilon_0)
     r1: np.ndarray = r - chargePos
     r1MagCubed: float = np.linalg.norm(r1, axis=axis)**3
-    force = k*np.divide(r1, r1MagCubed[..., np.newaxis])
+    force = k * np.divide(r1, r1MagCubed[..., np.newaxis])
 
     return force
 
-def DrawVectorField(surface: pygame.surface, xVectors: int, yVectors: int, chargeArray: np.ndarray, vectorLength: float) -> None:
+
+def DrawVectorField(surface: pygame.surface, xVectors: int, yVectors: int,
+                    chargeArray: np.ndarray, vectorLength: float) -> None:
     colour: tuple = (200, 200, 200)
 
     windowWidth: int
@@ -32,8 +36,8 @@ def DrawVectorField(surface: pygame.surface, xVectors: int, yVectors: int, charg
     xPositions: np.ndarray = np.linspace(0, windowWidth, num=xVectors)
     yPositions: np.ndarray = np.linspace(0, windowHeight, num=yVectors)
 
-    positionGrid: np.ndarray = np.stack(np.meshgrid(xPositions, yPositions), axis=-1)
-    
+    positionGrid: np.ndarray = np.stack(np.meshgrid(xPositions, yPositions),
+                                        axis=-1)
 
     forceField: np.ndarray = np.zeros(positionGrid.shape, dtype=np.float64)
 
@@ -46,9 +50,10 @@ def DrawVectorField(surface: pygame.surface, xVectors: int, yVectors: int, charg
             forceMag: float = np.linalg.norm(force)
             startPos: np.ndarray = [xPos, yPos]
 
-            endPos: np.ndarray = startPos + vectorLength*force/forceMag
+            endPos: np.ndarray = startPos + vectorLength * force / forceMag
 
             pygame.draw.line(surface, colour, startPos, endPos)
+
 
 def DrawCharges(surface: pygame.surface, chargeArray: np.ndarray) -> None:
     colour = (200, 200, 200)
@@ -58,30 +63,47 @@ def DrawCharges(surface: pygame.surface, chargeArray: np.ndarray) -> None:
 
 
 def DrawFieldLines(surface: pygame.surface, chargeArray: np.ndarray) -> None:
-    linesPerUnitCharge = 20
+    linesPerUnitCharge = 4
     for charge in chargeArray:
-        lines = np.abs(charge[0])*linesPerUnitCharge
+        lines = np.abs(charge[0]) * linesPerUnitCharge
         for i in range(lines):
             DrawFieldLine(surface, charge, i, lines, chargeArray)
 
-def DrawFieldLine(surface: pygame.surface, charge: np.ndarray, i: int, totLines: int, chargeArray: np.ndarray) -> None:
-    dl: float = 10
+
+def DrawFieldLine(surface: pygame.surface, charge: np.ndarray, i: int,
+                  totLines: int, chargeArray: np.ndarray) -> None:
+    dl: float = 20
     r: float = 5
-    colour: tuple = (200,200,200)
-    initOffset: np.ndarray = np.array([r*np.cos(i*2*np.pi/totLines), r*np.sin(i*2*np.pi/totLines)])
+    colour: tuple = (200, 200, 200)
+    initOffset: np.ndarray = np.array([
+        r * np.cos(i * 2 * np.pi / totLines),
+        r * np.sin(i * 2 * np.pi / totLines)
+    ])
     startPos: np.ndarray = np.array(charge[1] + initOffset)
-    for i in range(10):
+    ended: bool = False
+    while not ended:
         force: np.ndarray = np.zeros((2), dtype=np.float64)
         for chargeForce in chargeArray:
-            force += CoulombForceField(startPos, chargeForce[0], chargeForce[1])
+            force += CoulombForceField(startPos, chargeForce[0],
+                                       chargeForce[1])
         forceMag: float = np.linalg.norm(force)
-        endPos: np.ndarray = startPos + (force*dl)/forceMag
+        endPos: np.ndarray = startPos + (force * dl) / forceMag
         pygame.draw.line(surface, colour, startPos, endPos)
         startPos = endPos
+        if endPos[0] > 1500 or endPos[0] < -1500 or endPos[1] > 1500 or endPos[
+                1] < -1500:
+            ended = True
 
-chargeArray: np.ndarray = np.array([[-2, [100,500]], [1, [400,300]], [2, [700,500]]], dtype=object)
+        for charge in chargeArray:
+            if charge[0] < 0:
+                if np.linalg.norm(charge[1] - endPos) < r:
+                    ended = True
 
-chargePos = np.array((400,300))
+
+chargeArray: np.ndarray = np.array(
+    [[-2, [100, 500]], [1, [400, 300]], [2, [700, 500]]], dtype=object)
+
+chargePos = np.array((400, 300))
 chargeQ = -1
 
 windowWidth, windowHeight = 800, 600
@@ -103,7 +125,8 @@ while True:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 for chargeIndex, charge in enumerate(chargeArray):
-                    if np.linalg.norm(np.array(pygame.mouse.get_pos()) - charge[1]) < 10:
+                    if np.linalg.norm(
+                            np.array(pygame.mouse.get_pos()) - charge[1]) < 10:
                         dragging = True
                         dragger = chargeIndex
         elif event.type == pygame.MOUSEBUTTONUP:
@@ -114,13 +137,13 @@ while True:
 
     if dragging:
         chargeArray[dragger][1] = pygame.mouse.get_pos()
-    
-    screen.fill((0,0,0))
+
+    screen.fill((0, 0, 0))
 
     mouseX, mouseY = pygame.mouse.get_pos()
 
     #chargePos = np.array([mouseX, mouseY])
-    
+
     #chargeArray[2] = [chargeQ, chargePos]
 
     #DrawVectorField(screen, 50, 50, chargeArray, 10)
